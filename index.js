07cf3a6e2df14bb6495f101d0cc91bd4e6cbc4d0
Manuel <mzamorad95@gmail.com>
2018-02-06 18:40:51 +0100

    Inicio

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const token = '532721493:AAGCXkOh3XkjIPsRmbT1l0xri0tLvtn7yzI';
 const bot = new TelegramBot(token, {polling: true});
 const request = require('request');
 
-// Cuando mandes el mensaje "Hola" reconoce tú nombre y genera un input: Hola Daniel
 bot.on('message', (msg) => {
 if (msg.text.toString().toLowerCase().indexOf("sao") === 0) {
-bot.sendMessage(msg.chat.id, "SAO mi patria, JoJo mi castigo.");
+bot.sendMessage(msg.chat.id, "Sao mi patria, JoJo mi castigo.");
 }
 });
