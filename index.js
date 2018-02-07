f6c047a566020fbfb3697a46337136df2c062e9f
Manuel <mzamorad95@gmail.com>
2018-02-07 09:51:50 +0100

    make it better

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const token = '532721493:AAGCXkOh3XkjIPsRmbT1l0xri0tLvtn7yzI';
 // Creamos un bot que usa 'polling'para obtener actualizaciones
 const bot = new TelegramBot(token, {polling: false});
 const request = require('request');
+const sticker_mal: CAADBAADfAADEKvrDTL6OqJQP9JYAg
 
 bot.on('message', (msg) => {
 if (msg.text.toString().toLowerCase().indexOf("sao") === 0) {
 	bot.sendMessage(msg.chat.id, "Sao mi patria, JoJo mi castigo.");
+}
+if (msg.text.toString().toLowerCase() === "/mal") {
+	bot.sendMessage(msg.chat.id, "", sticker_mal);
 }
 });
