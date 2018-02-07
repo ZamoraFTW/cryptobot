7d212318f055e67639a88c07c12992764b5f9fce
Manuel <mzamorad95@gmail.com>
2018-02-07 09:58:02 +0100

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const TelegramBot = require('node-telegram-bot-api');
 
 // API Token Telegram
 const token = '532721493:AAGCXkOh3XkjIPsRmbT1l0xri0tLvtn7yzI';
-const sticker_mal = "CAADBAADfAADEKvrDTL6OqJQP9JYAg";
 // Creamos un bot que usa 'polling'para obtener actualizaciones
 const bot = new TelegramBot(token, {polling: false});
 const request = require('request');
@@ -11,8 +10,5 @@ const request = require('request');
 bot.on('message', (msg) => {
 if (msg.text.toString().toLowerCase().indexOf("sao") === 0) {
 	bot.sendMessage(msg.chat.id, "Sao mi patria, JoJo mi castigo.");
-}
-if (msg.text.toString().toLowerCase() === "/mal") {
-	bot.sendMessage(msg.chat.id, "", sticker_mal);
 }
 });
